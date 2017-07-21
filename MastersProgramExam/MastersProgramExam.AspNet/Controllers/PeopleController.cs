d80ed782887afb57b7ccaa5942d97e47572d8a6f
Chingiz <olzhabaev.sh@outlook.com>
2017-07-21 12:11:10 +0600

    make API

--- MastersProgramExam/MastersProgramExam.AspNet/Controllers/PeopleController.cs
+++ MastersProgramExam/MastersProgramExam.AspNet/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MastersProgramExam.AspNet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,45 @@ namespace MastersProgramExam.AspNet.Controllers
 {
     public class PeopleController : ApiController
     {
+        [HttpGet]
+        public ICollection<PersonVM> GetPeople(int startId = 0, int pageSize = 5)
+        {
+            var res = new List<PersonVM>();
+
+            var adapter = new Models.HomebookDatasetTableAdapters.Get_PeopleTableAdapter();
+
+            var data = adapter.GetData(startId, pageSize);
+            
+            for(int i = 0; i < data.Rows.Count; i++)
+            {
+                res.Add(new PersonVM()
+                {
+                    Id = data[i].Id,
+                    Firstname = data[i].Firstname,
+                    Lastname = data[i].Lastname,
+                    RegionId = data[i].Id1,
+                    Title = data[i].Title
+                });
+            }
+
+            return res;
+        }
+
+        [HttpPut]
+        public void UpdatePerson(PersonVM prs)
+        {
+            var adapter = new Models.HomebookDatasetTableAdapters.QueriesTableAdapter();
+
+            adapter.Update_Person(prs.Id, prs.Firstname, prs.Lastname, prs.RegionId);
+        }
+
+        [HttpDelete]
+        public void DeletePerson(int id)
+        {
+            var adapter = new Models.HomebookDatasetTableAdapters.QueriesTableAdapter();
+
+            adapter.Delete_Person(id);
+        }
+
     }
 }
---
+++ MastersProgramExam/MastersProgramExam.AspNet/Models/PersonVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MastersProgramExam.AspNet.Models
+{
+    public class PersonVM
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public int RegionId { get; set; }
+
+        public string Title { get; set; }
+    }
+}
